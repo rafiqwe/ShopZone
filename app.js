1c8fd5eb5f70daf6baf210a5316fc936edaeec91
rafiqwe <rmlmanik502@gmail.com>
2025-06-16 07:32:56 +0600

    install dotenv pkg

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+require('dotenv').config()
 
 const port = 3000;
 
