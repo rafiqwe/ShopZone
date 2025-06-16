443413cb1feea645d5f3395aae085087a3d00819
rafiqwe <rmlmanik502@gmail.com>
2025-06-16 12:36:31 +0600

    Create register page and Install ejs

--- app.js
+++ app.js
@@ -8,17 +8,26 @@ const app = express();
 const ownersRouter = require("./routes/ownersRouter");
 const usersRouter = require("./routes/usersRouter");
 const productRouter = require("./routes/productsRouter");
+const expressSeccion = require("express-session");
+const flash = require("connect-flash");
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  expressSeccion({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'secret',
+  })
+);
+app.use(flash());
+
 app.use(express.static(path.join(__dirname, "public")));
-require('dotenv').config()
+require("dotenv").config();
 
 const port = 3000;
-
-
 
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/authController");
+
 router.get("/", (req, res) => {
-  res.send("hey man");
+  res.render("register");
 });
 
 router.post("/register", registerUser);
-router.post('/login', loginUser)
+router.post("/login", loginUser);
 module.exports = router;
