f19dc2b3cf0d52539041ec66236f994948a52dbe
rafiqwe <rmlmanik502@gmail.com>
2025-06-17 14:27:33 +0600

    Create shop route and write backend code

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const productModel = require('../models/product-model');
+const productModel = require("../models/product-model");
+const isLoggedIn = require("../middlewares/isLoggedIn");
 
-router.get("/", (req, res) => {
-  res.render("shop");
+router.get("/", isLoggedIn, async (req, res) => {
+  try {
+    const allProducts = await productModel.find();
+    // Group by category
+    const manProducts = allProducts.filter((p) => p.category === "man");
+    const womanProducts = allProducts.filter((p) => p.category === "woman");
+    const childProducts = allProducts.filter((p) => p.category === "child");
+    res.render("shop", {
+      manProducts,
+      womanProducts,
+      childProducts,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
 });
-
 
 module.exports = router;
