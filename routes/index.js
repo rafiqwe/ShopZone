a581c5b5a2527c0f991a4a30d04bc5ef0af41bec
rafiqwe <rmlmanik502@gmail.com>
2025-06-18 11:04:43 +0600

    create sort by low to hight or hight to low price

--- routes/index.js
+++ routes/index.js
@@ -18,25 +18,53 @@ router.get("/cart", isLoggedIn, async (req, res) => {
     .findOne({ email: req.user.email })
     .populate("cart");
   const cart = user.cart;
-  const total = cart.forEach(item => {
+  const total = cart.forEach((item) => {
     console.log();
-
   });
-  
+
   res.render("cart", { cart });
+});
+
+router.post("/cart/remove", isLoggedIn, async (req, res) => {
+  const { productId } = req.body;
+
+  // Find the user
+  const user = await userModel.findOne({ email: req.user.email });
+
+  // Filter out the product you want to remove
+  user.cart = user.cart.filter((item) => item._id.toString() !== productId);
+
+  // Save the updated user
+  await user.save();
+
+  // Send back the updated cart
+  res.redirect("/cart"); // or res.send(user.cart);
 });
 
 router.get("/shop", isLoggedIn, async (req, res) => {
   try {
-    const allProducts = await productModel.find();
+    const sort = req.query.sort; // "low" or "high"
+    let sortOption = {};
+
+    if (sort === "low") {
+      sortOption = { price: 1 }; // Ascending
+    } else if (sort === "high") {
+      sortOption = { price: -1 }; // Descending
+    }
+
+    // Get all products with optional sorting
+    const allProducts = await productModel.find().sort(sortOption);
+
     // Group by category
     const manProducts = allProducts.filter((p) => p.category === "man");
     const womanProducts = allProducts.filter((p) => p.category === "woman");
     const childProducts = allProducts.filter((p) => p.category === "child");
+
     res.render("shop", {
       manProducts,
       womanProducts,
       childProducts,
+      sort,
     });
   } catch (error) {
     console.error(error.message);
