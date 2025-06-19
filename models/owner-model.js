b73d58306f538967d55cae588186e8557b2bbad3
rafiqwe <rmlmanik502@gmail.com>
2025-06-19 10:19:12 +0600

    Fixed product schema

--- models/owner-model.js
+++ models/owner-model.js
@@ -4,10 +4,12 @@ const ownerSchema = mongoose.Schema({
   fullName: String,
   email: String,
   password: String,
-  product: {
-    type: Array,
-    default: [],
-  },
+  product: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "product",
+    },
+  ],
   picture: Buffer,
   gstin: String,
 });
