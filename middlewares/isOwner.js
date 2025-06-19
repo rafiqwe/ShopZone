02f51750d91ac5a6ec64310abc7d9ff9208a7967
rafiqwe <rmlmanik502@gmail.com>
2025-06-19 10:18:27 +0600

    fixed some issues

--- middlewares/isOwner.js
+++ middlewares/isOwner.js
@@ -20,30 +20,3 @@ const isOwner = (req, res, next) => {
 };
 
 module.exports = isOwner;
-
-// const Owner = require("../models/owner-model");
-
-// const isOwner = async (req, res, next) => {
-//   try {
-//     // Step 1: Check if user is logged in
-//     if (!req.user) {
-//       return res.status(401).send("Unauthorized. Please log in.");
-//     }
-
-//     // Step 2: Find owner by email from logged in user
-//     const owner = await Owner.findOne({ email: req.user.email });
-
-//     // Step 3: If not found, block access
-//     if (!owner) {
-//       return res.status(403).send("Access Denied. Only Owner can access this page.");
-//     }
-
-//     // Step 4: Owner found, move to next step
-//     next();
-//   } catch (err) {
-//     console.error("isOwner middleware error:", err.message);
-//     res.status(500).send("Server error");
-//   }
-// };
-
-// module.exports = isOwner;
