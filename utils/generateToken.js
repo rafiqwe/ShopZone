650657492543af988ff0f734d6ed6228b9ec2635
rafiqwe <rmlmanik502@gmail.com>
2025-06-19 08:54:57 +0600

    Create Generate owner token

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,4 +4,12 @@ const generateToken = (user) => {
   return jwt.sign({ email: user.email, id: user._id }, process.env.JWT_KEY);
 };
 
+// const jwt = require("jsonwebtoken");
+
+const ownerGenerateToken = (owner) => {
+  return jwt.sign({ email: owner.email, id: owner._id }, process.env.OWNER_JWT);
+};
+
+module.exports.ownerGenerateToken = ownerGenerateToken;
+
 module.exports.generateToken = generateToken;
