c20dafdcd160e6efbcb7832099c9214643654aed
rafiqwe <rmlmanik502@gmail.com>
2025-06-22 22:20:36 +0600

    nothing

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, logout } = require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+  logout,
+} = require("../controllers/authController");
 
-router.get("/", (req, res) => {
-  res.render("register", {loggedIn : true});
+router.get("/register", (req, res) => {
+  res.render("register", { loggedIn: true });
 });
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get('/logout', logout)
+router.get("/logout", logout);
 module.exports = router;
